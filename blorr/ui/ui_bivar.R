1f751540835bc8844c8cdc7bd28ab6520288995d
rsquaredin <rsquaredin@gmail.com>
2019-03-12 10:11:48 +0530

    eval does not work in shiny

--- blorr/ui/ui_bivar.R
+++ blorr/ui/ui_bivar.R
@@ -8,8 +8,8 @@ tabPanel('Bivariate Analysis', value = 'tab_bivar', icon = icon('cubes'),
         source('ui/ui_woe_iv.R', local = TRUE)[[1]],
         source('ui/ui_woe_iv_stats.R', local = TRUE)[[1]],
         source('ui/ui_segment_dist.R', local = TRUE)[[1]],
-        source('ui/ui_2way_segment.R', local = TRUE)[[1]]
-        # source('ui/ui_bivar_analysis.R', local = TRUE)[[1]]
+        source('ui/ui_2way_segment.R', local = TRUE)[[1]],
+        source('ui/ui_bivar_analysis.R', local = TRUE)[[1]]
 				
     )
 )
